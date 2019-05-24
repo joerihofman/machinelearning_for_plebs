795c576e1ae2290c982bf01fc9660d6b4f0d6c5e
hofman.j <hofman.j@tkppensioen.nl>
2019-05-24 09:13:28 +0200

    reg verbeterd

--- nl/joerihofman/machinelearningforplebs/linreg.py
+++ nl/joerihofman/machinelearningforplebs/linreg.py
@@ -6,11 +6,15 @@ from sklearn.linear_model import LinearRegression
 
 from nl.joerihofman.machinelearningforplebs.FileLoader import FileLoader
 
+START_EPOCH = datetime.date(1970, 1, 1)
+SEED = 7
+LM = LinearRegression()
+
 
 def date_time_to_days_since_epoch(date):
     try:
         parsed_date = datetime.datetime.strptime(date, "%d-%m-%y %H:%M").date()
-        return (parsed_date - datetime.date(1970, 1, 1)).days
+        return (parsed_date - START_EPOCH).days
     except ValueError:
         return date_to_days_since_epoch(date)
 
@@ -18,64 +22,62 @@ def date_time_to_days_since_epoch(date):
 def date_to_days_since_epoch(date):
     try:
         parsed_date = datetime.datetime.strptime(date, "%d-%m-%y").date()
-        return (parsed_date - datetime.date(1970, 1, 1)).days
+        return (parsed_date - START_EPOCH).days
     except TypeError:
         raise
 
 
-def bereken_die_zooi(aanlever, tijdvak):
+def calculate_difference_in_days(aanlever, tijdvak):
     try:
-        aanl = date_time_to_days_since_epoch(aanlever)
-        tijdv = date_to_days_since_epoch(tijdvak)
-        return aanl - tijdv
+        aanl_aantal_dagen = date_time_to_days_since_epoch(aanlever)
+        tijdv_aantal_dagen = date_to_days_since_epoch(tijdvak)
+        return aanl_aantal_dagen - tijdv_aantal_dagen
     except TypeError:
-        return 1000000000
+        return 100000000
 
 
-# date_to_days_since_epoch(row[8]) - date_time_to_days_since_epoch(row[3])
+def create_dataframe():
+    dataset = FileLoader.get_dataset_from_file("data_export_zonder_header.csv")
+    data_frame = pandas.DataFrame(dataset)
+    data_frame["verschil"] = data_frame.apply(lambda row: calculate_difference_in_days(row[3], row[8]), axis=1)
 
-class LinReg:
-    seed = 7
+    data_frame = data_frame[data_frame.verschil != 100000000]
 
-    lm = LinearRegression()
-
-    @staticmethod
-    def maak_dataframe():
-        dataset = FileLoader.get_dataset_from_file("data_export_zonder_header.csv")
-        data_frame = pandas.DataFrame(dataset)
-        data_frame["verschil"] = data_frame.apply(lambda row: bereken_die_zooi(row[3], row[8]), axis=1)
-
-        data_frame = data_frame[data_frame.verschil != 1000000000]
-
-        data_frame["tijdvakbegin_epoch"] = data_frame.apply(lambda row: date_to_days_since_epoch(row[8]), axis=1)
-        data_frame["ontvangst_epoch"] = data_frame.apply(lambda row: date_time_to_days_since_epoch(row[3]), axis=1)
-        return data_frame
-
-    @staticmethod
-    def print_dit(data_frame):
-        for it in data_frame["verschil"]:
-            print(it)
-
-    def schatting_samenhang(self, data_frame):
-        Y = data_frame[["tijdvakbegin_epoch", "ontvangst_epoch", "psn_id_fonds"]].copy()  # .drop("verschil", axis=1).drop("id_extern", axis=1)
-        Y = Y.fillna(data_frame.mean())
-        X = data_frame[["verschil"]].copy().fillna(data_frame["verschil"].mean())
-        self.lm.fit(X, Y)
-        return pandas.DataFrame(zip(Y.columns, self.lm.coef_), columns=["features", "estimatedcoef"])
-
-    def print_scatterplot(self, data_frame):
-        plt.scatter(data_frame.tijdvakbegin_epoch, data_frame.verschil)
-        plt.ylabel("verschil in dagen")
-        plt.xlabel("tijdvak begin")
-        plt.title("titel")
-        plt.show()
+    data_frame["tijdvakbegin_epoch"] = data_frame.apply(lambda row: date_to_days_since_epoch(row[8]), axis=1)
+    data_frame["ontvangst_epoch"] = data_frame.apply(lambda row: date_time_to_days_since_epoch(row[3]), axis=1)
+    return data_frame
 
 
-lr = LinReg()
-dataframe = lr.maak_dataframe()
-print(dataframe.shape)
-print(lr.schatting_samenhang(dataframe))
-lr.print_scatterplot(dataframe)
+def estimate_coefficient(data_frame):
+    x = data_frame[["verschil"]].copy().fillna(data_frame["verschil"].mean())
+    y = data_frame[["tijdvakbegin_epoch", "ontvangst_epoch", "psn_id_fonds"]].copy()
+    y = y.fillna(data_frame.mean())
+    LM.fit(x, y)
+    plt.scatter(data_frame.tijdvakbegin_epoch, LM.predict(x))
+    plt.ylabel("Verschil aanlevermoment en tijdvak (dagen)")
+    plt.xlabel("Begin tijdvak (dagen sinds epoch)")
+    plt.title("Scatterplot onderzoek")
+    plt.show()
+    return pandas.DataFrame(zip(y.columns, LM.coef_), columns=["features", "estimatedcoef"])
+
+
+def make_prediction(data_frame):
+    x = data_frame[["verschil"]].copy().fillna(data_frame["verschil"].mean())
+
+
+def print_scatterplot(data_frame):
+    plt.scatter(data_frame.tijdvakbegin_epoch, data_frame.verschil)
+    plt.ylabel("Verschil aanlevermoment en tijdvak (dagen)")
+    plt.xlabel("Begin tijdvak (dagen sinds epoch)")
+    plt.title("Scatterplot onderzoek")
+    plt.show()
+
+
+dataframe = create_dataframe()
+make_prediction(dataframe)
+estimate_coefficient(dataframe)
+# print(estimate_coefficient(dataframe))
+# print_scatterplot(dataframe)
 
 
 
