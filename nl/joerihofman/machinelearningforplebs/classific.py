61bf0880f5b278b5fd00870b646eef7293f6cd13
hofman.j <hofman.j@tkppensioen.nl>
2019-05-31 15:10:05 +0200

    begin classificatie

--- nl/joerihofman/machinelearningforplebs/classific.py
+++ nl/joerihofman/machinelearningforplebs/classific.py
@@ -1,21 +1,93 @@
+import time
+
+import numpy
 import pandas
 import sklearn
+from IPython.core.display import display
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
 
 from nl.joerihofman.machinelearningforplebs.FileLoader import FileLoader
 
 
 def create_dataframe():
+    mapping = {"": 0, "Wacht op verwerking van aangifte voorgaand tijdvak": 1, "Wacht op verwerking van aangiftebericht met volledige aangifte": 2}
     dataset = FileLoader.get_dataset_from_fout_file("IO_Status_berichten_zonder_header.csv")
-    return pandas.DataFrame(dataset)
+    frame = pandas.DataFrame(dataset)
+    frame.replace({'NEW_REDEN': mapping})
+    return frame
 
 
 def make_testdata(data_frame):
     return sklearn.model_selection.train_test_split(
-        data_frame.OLD_STATUS, data_frame, test_size=0.33, random_state=5
+        data_frame.ID_OLD_EXTERN, data_frame.NEW_REDEN, test_size=0.33, random_state=5
     )
 
 
+dict_classifiers = {
+    "Logistic Regression": LogisticRegression(),
+    "Nearest Neighbors": KNeighborsClassifier(),
+    "Linear SVM": SVC(),
+    "Gradient Boosting Classifier": GradientBoostingClassifier(n_estimators=1000),
+    "Decision Tree": sklearn.tree.DecisionTreeClassifier(),
+    "Random Forest": RandomForestClassifier(n_estimators=1000),
+    "Neural Net": MLPClassifier(alpha = 1),
+    "Naive Bayes": GaussianNB(),
+    #"AdaBoost": AdaBoostClassifier(),
+    #"QDA": QuadraticDiscriminantAnalysis(),
+    #"Gaussian Process": GaussianProcessClassifier()
+}
+
+def batch_classify(X_train, Y_train, X_test, Y_test, no_classifiers = 1, verbose = True):
+    """
+    This method, takes as input the X, Y matrices of the Train and Test set.
+    And fits them on all of the Classifiers specified in the dict_classifier.
+    The trained models, and accuracies are saved in a dictionary. The reason to use a dictionary
+    is because it is very easy to save the whole dictionary with the pickle module.
+
+    Usually, the SVM, Random Forest and Gradient Boosting Classifier take quiet some time to train.
+    So it is best to train them on a smaller dataset first and
+    decide whether you want to comment them out or not based on the test accuracy score.
+    """
+
+    dict_models = {}
+    for classifier_name, classifier in list(dict_classifiers.items())[:no_classifiers]:
+        t_start = time.clock()
+        classifier.fit(X_train, Y_train)
+        t_end = time.clock()
+
+        t_diff = t_end - t_start
+        train_score = classifier.score(X_train, Y_train)
+        test_score = classifier.score(X_test, Y_test)
+
+        dict_models[classifier_name] = {'model': classifier, 'train_score': train_score, 'test_score': test_score, 'train_time': t_diff}
+        if verbose:
+            print("trained {c} in {f:.2f} s".format(c=classifier_name, f=t_diff))
+    return dict_models
+
+
+
+def display_dict_models(dict_models, sort_by='test_score'):
+    cls = [key for key in dict_models.keys()]
+    test_s = [dict_models[key]['test_score'] for key in cls]
+    training_s = [dict_models[key]['train_score'] for key in cls]
+    training_t = [dict_models[key]['train_time'] for key in cls]
+
+    df_ = pandas.DataFrame(data=numpy.zeros(shape=(len(cls),4)), columns = ['classifier', 'train_score', 'test_score', 'train_time'])
+    for ii in range(0,len(cls)):
+        df_.loc[ii, 'classifier'] = cls[ii]
+        df_.loc[ii, 'train_score'] = training_s[ii]
+        df_.loc[ii, 'test_score'] = test_s[ii]
+        df_.loc[ii, 'train_time'] = training_t[ii]
+
+    display(df_.sort_values(by=sort_by, ascending=False))
+
 dataframe = create_dataframe()
 x_train, x_test, y_train, y_test = make_testdata(dataframe)
+batch_classify(x_train, y_train, x_test, y_test)
 print(y_train)
 print(x_train)
